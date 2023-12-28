da147acfe4d05fd626b3328ecc099e7c4a3282a4
Mariulus Marcos de Almeida <mariulus_kol@hotmail.com>
2023-12-28 12:46:04 -0300

    Update desafio.js

--- desafio.js
+++ desafio.js
@@ -1,21 +1,21 @@
 let heroi = "Lampião"
-let xp = 11000 //Quantidade de experiência do herói
+let xp = 1001 //Quantidade de experiência do herói
 let nivel = ""
 if(xp <= 1000) {
 nivel = "Ferro"
-}else if(xp >= 1001 && xp <= 2000) {
+}else if(xp <= 2000) {
 nivel = "Bronze"
-}else if(xp >= 2001 && xp <= 5000) {
+}else if(xp <= 5000) {
 nivel = "Prata"
-}else if(xp >= 5001 && xp <= 7000) {
+}else if(xp <= 7000) {
 nivel = "Ouro"
-}else if(xp >= 7001 && xp <= 8000) {
+}else if(xp <= 8000) {
 nivel = "Platina"
-}else if(xp >= 8001 && xp <= 9000) {
+}else if(xp <= 9000) {
 nivel = "Ascendente"
-}else if(xp >= 9001 && xp <= 10000) {
+}else if(xp <= 10000) {
 nivel = "Imortal"
-}else if(xp >= 10001) {
+}else{
 nivel = "Radiante"
 }
 console.log("O Herói de nome " + heroi + " está no nível de " + nivel + ".")
